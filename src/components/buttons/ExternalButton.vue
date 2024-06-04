f61fbd96bb25308553c871b908ae9710f5297a9f
WukoSiric <lstainer2002@gmail.com>
2024-06-04 18:02:16 +0930

    Inner text is optional

--- src/components/buttons/ExternalButton.vue
+++ src/components/buttons/ExternalButton.vue
@@ -19,8 +19,10 @@ export default {
     class="center m-4 box-border flex w-fit select-none items-center justify-center gap-2 rounded-lg bg-offwhite p-2 px-8 shadow-lg transition-all duration-75 ease-in-out hover:scale-105 hover:cursor-pointer hover:bg-slate-300"
   >
     <img class="size-10" :src="img" />
-    <a class="text-nowrap font-mono text-xl font-bold text-black hover:underline"
-      ><slot> Go to </slot>
-    </a>
+    <a
+      v-if="$slots.default"
+      class="text-nowrap font-mono text-xl font-bold text-black hover:underline"
+      ><slot></slot
+    ></a>
   </a>
 </template>
